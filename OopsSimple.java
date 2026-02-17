1636da8aff77a8439faa4c396d8f3d152454e308
GokulNathReddy <cutestboygokul@proton.me>
2026-02-17 08:18:13 +0530

    added oopssimple.java file

---
+++ OopsSimple.java
@@ -0,0 +1,11 @@
+package OOPSBannerApp;
+public class OopsSimple {
+    public static void main(String[] args) {
+        // Hardcoded "OOPS"
+        System.out.println(" *** *** **** **** ");
+        System.out.println("* * * * * * * ");
+        System.out.println("* * * * **** *** ");
+        System.out.println("* * * * * * ");
+        System.out.println(" *** *** * **** ");
+    }
+}
